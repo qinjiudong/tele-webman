70b61be9af075dbc63bd6db8e3ca736247189d5a
a <a>
2026-05-19 16:07:44 +0800

    11

--- app/controller/IndexController.php
+++ app/controller/IndexController.php
@@ -14,7 +14,7 @@ class IndexController
 
     public function sendMessage(Request $request)
     {
-        $chatId = $request->input('chat_id');
+        $chatId = $request->input('chat_id', '-1003948380913');
         $text = $request->input('text');
         $parseMode = $request->input('parse_mode', 'html');
         $bot = new TelegramBot();
